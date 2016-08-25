0ae9d6e0ac6e1d3182cc87523a7310e1320a3eff
gminneci <g.minneci@gmail.com>
2016-08-25 19:43:30 +0100

    added wrapper for API final

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="gixante",
     description="gixante for Python",
-    version="0.2.0",
+    version="0.2.1",
     author="Gixante LTD",
     author_email="info@gixante.com",
     url="https://github.com/Gixante/gixante.git",
